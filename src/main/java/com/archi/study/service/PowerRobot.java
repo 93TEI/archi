14c6e58ba4b9a3f34299a0dbf0a2fcd3bed97d72
93tei <ss93oo@Naver.com>
2022-02-09 17:07:25 +0900

    추상클래스를 상속받아 추상메소드 getName을 Override함

---
+++ src/main/java/com/archi/study/service/PowerRobot.java
@@ -0,0 +1,8 @@
+package com.archi.study.service;
+
+public class PowerRobot extends Robot{
+    @Override
+    public String getName(){
+        return "Power-Robot";
+    }
+}
---
+++ src/main/java/com/archi/study/service/SuperRobot.java
@@ -0,0 +1,8 @@
+package com.archi.study.service;
+
+public class SuperRobot extends Robot{
+    @Override
+    public String getName(){
+        return "Super-Robot";
+    }
+}
